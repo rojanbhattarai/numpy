a46ad8edc904f070cbe7aa5e4f6057d2e2cd528e
rojanbhattarai <bhattarairoj301@gmail.com>
2026-01-07 21:24:52 +0545

    third commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+marks=np.array([32,33,34,35,36])
+
+average=np.mean(marks)
+
+print(average)
---
+++ num.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+temperatures=np.array([32,33,34,35,36])
+average=np.mean(temperatures)
+print(average)
